a7f2b65a5dc772a1297297034255f234ccdad9a8
kelseylieberman <36453545+kelseylieberman@users.noreply.github.com>
2020-09-22 12:15:37 -0700

    Geometrical shapes for badges

---
+++ DrawingPathsAndShapes/DrawingPathsAndShapes/RotatedBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotatedBadgeSymbol.swift
+//  DrawingPathsAndShapes
+//
+//  Created by Kelsey Lieberman on 9/22/20.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: .init(degrees: 5))
+    }
+}
